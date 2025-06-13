ddd7e75ae3108025267ddb30befe35fd0139e52e
aniketaerion <info@aerionaerospace.io>
2025-06-13 11:40:41 +0530

    Fix: Correct logo path in LoginPage

--- src/pages/login/LoginPage.jsx
+++ src/pages/login/LoginPage.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
-import supabase from "../supabaseClient";
-import logo from "../assets/logo.png";
+import supabase from "@/supabaseClient";
+import logo from "@/assets/logo.png";
 
 function LoginPage() {
   const navigate = useNavigate();
